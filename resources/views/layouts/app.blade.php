478934c0c08a66b3578d151d83275a4c045e1e99
Vlad Vasiliev <v14d06@gmail.com>
2024-10-12 10:38:10 +0300

    fix: fixed error null ref in main page MS-15

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,16 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />-->
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
 <div class="min-vh-100 bg-light">
     @include('layouts.navigation')
 
-    <!-- Page Heading -->
     @isset($header)
         <header class="bg-white shadow">
             <div class="container py-4">
@@ -28,7 +25,6 @@
         </header>
     @endisset
 
-    <!-- Page Content -->
     <main>
         @yield('content')
     </main>
