fb40a4fbe5126b1798dc66e38d7f69d4afd2addc
Vlad Vasiliev <v14d06@gmail.com>
2024-11-14 17:45:27 +0200

    MS-18 feat: add cart for products

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products/{id}', [HomeController::class, 'details'])->name('products.details');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/update/{productId}', [CartController::class, 'update'])->name('cart.update');
+    Route::post('/cart/remove/{productId}', [CartController::class, 'remove'])->name('cart.remove');
+    
     Route::get('/dashboard', function () {
         return view('admin.dashboard');
     })->name('dashboard');
 
-    Route::prefix('admin')->middleware('admin')->group(function () {
+    Route::prefix('admin')->group(function () {
         Route::get('/', function () {
             return view('admin.dashboard');
         })->name('admin.dashboard');
