6153cddaa96a3057bd5b56f9d28cb40f67243069
Vlad Vasiliev <v14d06@gmail.com>
2024-11-15 10:37:16 +0200

    minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,8 +20,15 @@
 
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($products as $product)
-                <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <img class="w-full h-56 object-cover" src="{{ asset($product->image) }}" alt="{{ $product->name }}">                    <div class="p-4">
+                <div class="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-full">
+                    <div class="aspect-w-16 aspect-h-9 w-full">
+                        <img 
+                            class="w-full h-64 object-cover object-center" 
+                            src="{{ asset($product->image) }}" 
+                            alt="{{ $product->name }}"
+                        >
+                    </div>
+                    <div class="p-4 flex-grow">
                         <h5 class="text-lg font-semibold">{{ $product->name }}</h5>
                         <p class="text-gray-700 text-sm mt-2">{{ Str::limit($product->description, 80) }}</p>
                         <p class="text-gray-900 font-bold mt-4">Price: ${{ $product->price }}</p>
